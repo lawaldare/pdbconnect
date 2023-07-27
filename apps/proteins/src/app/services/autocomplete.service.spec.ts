712b003cf8dd2663853275b794c28e9c09fca17b
Sreenath Sasidharan Nair <sreenath@ebi.ac.uk>
2023-07-27 13:46:06 +0000

    Merging CICD changes to main

--- apps/proteins/src/app/services/autocomplete.service.spec.ts
+++ apps/proteins/src/app/services/autocomplete.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AutocompleteService } from './autocomplete.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AutocompleteService', () => {
   let service: AutocompleteService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(AutocompleteService);
   });
 
---
+++ bin/index.js
@@ -0,0 +1,66 @@
+const { writeFileSync } = require('fs');
+
+const createBaseFile = () => `
+stages:
+    - publish
+    - deploy
+`;
+
+const createEmptyJob = () => `
+publish:empty:
+    stage: publish
+    script:
+        - echo "No jobs affected by this commit"
+`;
+
+const createBuildJob = (serviceName) => `
+publish:${serviceName}:
+    stage: publish
+    needs:
+      - pipeline: $PARENT_PIPELINE_ID
+        job: build
+    script:
+      - docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+      - docker build --pull --cache-from $CI_REGISTRY_IMAGE --build-arg APP_BASE_HREF=\$${serviceName.toUpperCase()}_BASE_HREF --tag $CI_REGISTRY_IMAGE/${serviceName} --file apps/${serviceName}/Dockerfile .
+      - docker push $CI_REGISTRY_IMAGE/${serviceName}
+    tags:
+      - pdbe-shell
+`;
+
+const createDeployTrigger = (serviceName) => `
+trigger_deploy:${serviceName}:
+    stage: deploy
+    inherit:
+      variables: false
+    variables:
+      UPSTREAM_REF: main
+      IMAGE_PATHS: $CI_REGISTRY_IMAGE/${serviceName}
+      IMAGE_NAMES: ${serviceName}-pages
+      APP_NAME: ${serviceName}-pages
+    trigger:
+      project: pdbe/backend/k8s-deploy-configs
+      branch: main
+      strategy: depend
+    needs:
+      - publish:${serviceName}  
+`;
+
+const createCIFile = (projects) => {
+  if (!projects.length) {
+    return createBaseFile().concat(createEmptyJob());
+  }
+
+  return createBaseFile().concat(projects.map(createBuildJob).join('\n')).concat(projects.map(createDeployTrigger).join('\n'));
+};
+
+const createDynamicGitLabFile = () => {
+  const [stringifiedAffected] = process.argv.slice(2);
+
+  const { projects } = JSON.parse(stringifiedAffected);
+
+  const content = createCIFile(projects);
+
+  writeFileSync('dynamic-gitlab-ci.yml', content);
+};
+
+createDynamicGitLabFile();
