8668b249f793535b2319171775d5c7e6eca427db
marcelo <marcelo@ebi.ac.uk>
2023-10-26 11:09:43 +0100

    create story for component and added latest css import

--- libs/pdbe/swagger-docs/src/lib/pdbe-swagger-docs/pdbe-swagger-docs.component.stories.ts
+++ libs/pdbe/swagger-docs/src/lib/pdbe-swagger-docs/pdbe-swagger-docs.component.stories.ts
@@ -1,14 +1,40 @@
 import { Meta } from '@storybook/angular';
 import { PdbeSwaggerDocsComponent } from './pdbe-swagger-docs.component';
 
+
+const actionArgs = {
+  openApiURL: {
+    control: { type: 'text' },
+    description: 'OpenAPI Json file URL',
+  },
+  hostname: {
+    control: { type: 'text' },
+    description: 'Hostname of APIs',
+    table: {
+      defaultValue: { summary: 'EMBL-EBI' },
+    },
+  },
+  apiKeyValue: {
+    control: { type: 'text' },
+    description: 'API key',
+  }
+}
+
 export default {
   title: 'PDBe Components/Swagger Docs',
   component: PdbeSwaggerDocsComponent,
+  tags: ['autodocs'],
 } as Meta<PdbeSwaggerDocsComponent>;
 
 export const Primary = {
   render: (args: PdbeSwaggerDocsComponent) => ({
     props: args,
   }),
-  args: {},
+  args: {
+    openApiURL: 'https://wwwdev.ebi.ac.uk/pdbe/aggregated-api/openapi.json',
+    hostname: 'https://wwwdev.ebi.ac.uk/pdbe/aggregated-api/',
+    apiKeyValue: 'AIzaSyCeurAJz7ZGjPQUtEaerUkBZ3TaBkXrY94',
+  },
+  argTypes: actionArgs,
+  name: 'Default',
 };
