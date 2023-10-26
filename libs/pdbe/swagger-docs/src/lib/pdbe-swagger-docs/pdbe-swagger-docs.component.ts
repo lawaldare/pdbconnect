eecef3a1f1708b8fedf222e6587c7861a3095dbd
marcelo <marcelo@ebi.ac.uk>
2023-10-26 08:41:11 +0100

    added code and swagger-ui-dist lib

--- libs/pdbe/swagger-docs/src/lib/pdbe-swagger-docs/pdbe-swagger-docs.component.ts
+++ libs/pdbe/swagger-docs/src/lib/pdbe-swagger-docs/pdbe-swagger-docs.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SwaggerUIBundle } from 'swagger-ui-dist';
+import { SwaggerUIStandalonePreset } from 'swagger-ui-dist';
 
 @Component({
   selector: 'pdbc-pdbe-swagger-docs',
@@ -8,4 +10,55 @@ import { CommonModule } from '@angular/common';
   templateUrl: './pdbe-swagger-docs.component.html',
   styleUrls: ['./pdbe-swagger-docs.component.scss'],
 })
-export class PdbeSwaggerDocsComponent {}
+export class PdbeSwaggerDocsComponent {
+  @Input() openApiURL: string = '';
+  @Input() hostname: string = '';
+  @Input() apiKeyValue: string = '';
+
+
+  ngOnInit(): void {
+    // const hostname = this.openApiURL.split("/").slice(0, -1).join("/");
+    // const apiKeyValue: string = 'AIzaSyCeurAJz7ZGjPQUtEaerUkBZ3TaBkXrY94';
+
+    function HideInfoUrlPlugin() {
+      return {
+        components: {
+          InfoUrl: function() { return null }
+        }
+      }
+    }
+
+    const ui = SwaggerUIBundle({
+      url: this.openApiURL,
+      dom_id: '#swagger-ui',
+      deepLinking: true,
+      presets: [
+        SwaggerUIBundle['presets'].apis,
+        SwaggerUIStandalonePreset,
+      ],
+      syntaxHighlight: {
+        activate: true,
+        theme: "nord"
+      },
+      plugins: [
+        SwaggerUIBundle['plugins'].DownloadUrl,
+        HideInfoUrlPlugin
+      ],
+      layout: "BaseLayout",
+      requestInterceptor: (req: any) => { // type should be Request but with url not as readonly
+        if ('method' in req) {
+          const [urlPath, urlParams] = req.url.split('?');
+          const apiUrl = urlPath.replace(this.hostname, `${ this.hostname }/api`);
+          const queryParams = new URLSearchParams(urlParams);
+          queryParams.set('key', this.apiKeyValue);
+
+          const newParams = queryParams.toString();
+
+          // req.url = newParams ? `${apiUrl}?${newParams}` : apiUrl;
+          req.url =`${apiUrl}?${newParams}`;
+        }
+        return req;
+      },
+    });
+  }
+}
