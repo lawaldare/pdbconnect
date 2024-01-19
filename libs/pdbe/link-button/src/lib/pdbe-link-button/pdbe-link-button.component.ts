72612139b590ff6bb8ae4aa2d47f891076319d6c
marcelo <marcelo@ebi.ac.uk>
2024-01-19 14:32:41 +0000

    created pdbe link buttons

---
+++ libs/pdbe/link-button/src/lib/pdbe-link-button/pdbe-link-button.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'pdbc-pdbe-link-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pdbe-link-button.component.html',
+  styleUrls: ['./pdbe-link-button.component.scss'],
+})
+export class PdbeLinkButtonComponent {
+  @Input() label = "";
+  @Input() url?: string;
+  @Input() fontStyle = "";
+  @Input() toEmitOnClick?: string;
+  @Output() linkButtonEvent = new EventEmitter<string>();
+
+  // Communicate with parent using:
+  // https://angular.io/guide/inputs-outputs#sending-data-to-a-parent-component
+  clickedLinkButton() {
+    // console.log("clicked!")
+    this.linkButtonEvent.emit(this.toEmitOnClick!);
+  }
+}
