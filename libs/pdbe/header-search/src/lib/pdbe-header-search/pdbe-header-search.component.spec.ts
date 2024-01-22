bf27dbcfd6847257b3eb382852e6693a64075dcb
marcelo <marcelo@ebi.ac.uk>
2024-01-22 10:52:42 +0000

    pdbe-search int tests fixes

--- libs/pdbe/header-search/src/lib/pdbe-header-search/pdbe-header-search.component.spec.ts
+++ libs/pdbe/header-search/src/lib/pdbe-header-search/pdbe-header-search.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
+import { PdbeChipsStyleDatum } from '@pdbe-lib/chips';
+
 import { PdbeHeaderSearchComponent } from './pdbe-header-search.component';
 
 describe('PdbeHeaderSearchComponent', () => {
@@ -21,8 +23,8 @@ describe('PdbeHeaderSearchComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should not show clear icon by default', () => {
-    expect(component.showClearIcon).toBeFalsy();
+  it('should show advanced button by default', () => {
+    expect(component.hasAdvancedSearch).toBeTruthy();
   });
 
   it('should placeholder property value should be the input placeholder', () => {
@@ -32,32 +34,58 @@ describe('PdbeHeaderSearchComponent', () => {
     expect(inputEle.placeholder).toEqual('Test placeholder');
   });
 
-  it('should toggle showClearIcon property depending on input text', fakeAsync(() => {
-    component.searchTermStream.next('AAAAA');
-    tick(300);
-    expect(component.showClearIcon).toBeTruthy();
-
-    component.searchTermStream.next('');
-    tick(300);
-    expect(component.showClearIcon).toBeFalsy();
-  }));
-
-  it('should display clear icon according to showClearIcon property', () => {
-    component.showClearIcon = true;
+  it('should display PDBe chips according to examples property', () => {
+    component.searchChipsStyle = new PdbeChipsStyleDatum();
+    component.searchButtonChipsType === "PDBe";
+    component.examples = [
+      {'label':'AAA', 'url': 'https://www.ebi.ac.uk/1'},
+      {'label':'BBB', 'url': 'https://www.ebi.ac.uk/2'},
+      {'label':'CCC', 'url': 'https://www.ebi.ac.uk/3'},
+    ]
     fixture.detectChanges();
-    const ele1 = fixture.nativeElement.querySelector('.search-clear-icon');
-    expect(ele1).toBeTruthy();
 
-    component.showClearIcon = false;
-    fixture.detectChanges();
-    const ele2 = fixture.nativeElement.querySelector('.search-clear-icon');
-    expect(ele2).toBeFalsy();
+    const anchorEles = fixture.nativeElement.querySelectorAll('pdbc-pdbe-chips > a');
+    expect(anchorEles.length).toEqual(3);
+
+    expect(anchorEles[0]).toBeTruthy();
+    expect(anchorEles[0].innerHTML).toEqual(" AAA ");
+    expect(anchorEles[0].href).toEqual("https://www.ebi.ac.uk/1");
+
+    expect(anchorEles[1]).toBeTruthy();
+    expect(anchorEles[1].innerHTML).toEqual(" BBB ");
+    expect(anchorEles[1].href).toEqual("https://www.ebi.ac.uk/2");
+
+    expect(anchorEles[2]).toBeTruthy();
+    expect(anchorEles[2].innerHTML).toEqual(" CCC ");
+    expect(anchorEles[2].href).toEqual("https://www.ebi.ac.uk/3");
   });
 
-  it('should display Beta', () => {
-    component.beta = true;
-    fixture.detectChanges();
-    const betaEle = fixture.nativeElement.querySelector('#beta');
-    expect(betaEle).toBeTruthy();
-  });
+  // it('should toggle showClearIcon property depending on input text', fakeAsync(() => {
+  //   component.searchTermStream.next('AAAAA');
+  //   tick(300);
+  //   expect(component.showClearIcon).toBeTruthy();
+
+  //   component.searchTermStream.next('');
+  //   tick(300);
+  //   expect(component.showClearIcon).toBeFalsy();
+  // }));
+
+  // it('should display clear icon according to showClearIcon property', () => {
+  //   component.showClearIcon = true;
+  //   fixture.detectChanges();
+  //   const ele1 = fixture.nativeElement.querySelector('.search-clear-icon');
+  //   expect(ele1).toBeTruthy();
+
+  //   component.showClearIcon = false;
+  //   fixture.detectChanges();
+  //   const ele2 = fixture.nativeElement.querySelector('.search-clear-icon');
+  //   expect(ele2).toBeFalsy();
+  // });
+
+  // it('should display Beta', () => {
+  //   component.beta = true;
+  //   fixture.detectChanges();
+  //   const betaEle = fixture.nativeElement.querySelector('#beta');
+  //   expect(betaEle).toBeTruthy();
+  // });
 });
