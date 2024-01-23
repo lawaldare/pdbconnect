baf2e15aa4e0449fcdec3523c64020d15ba5a1a3
marcelo <marcelo@ebi.ac.uk>
2024-01-23 12:20:01 +0000

    fixed lint, format, tests

--- apps/pdb-entry-pages/src/app/app.component.ts
+++ apps/pdb-entry-pages/src/app/app.component.ts
@@ -5,7 +5,6 @@ import { PdbeHeaderLogoMenuComponent } from '@pdbe-lib/header-logo-menu';
 import { PdbeHeaderSearchComponent } from '@pdbe-lib/header-search';
 import { VfEbiFooterComponent } from '@vf-lib/ebi-footer';
 
-
 @Component({
   standalone: true,
   imports: [PdbeHeaderLogoMenuComponent, PdbeHeaderSearchComponent, VfEbiFooterComponent, RouterModule],
