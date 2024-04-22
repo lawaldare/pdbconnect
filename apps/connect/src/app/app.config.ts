1c38b92ed381eee66c12ed9977086c2955f5f122
roshan <roshan@ebi.ac.uk>
2024-04-22 18:33:19 +0100

    added angular material

--- apps/connect/src/app/app.config.ts
+++ apps/connect/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), provideHttpClient()],
+  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), provideHttpClient(), provideAnimations()],
 };
