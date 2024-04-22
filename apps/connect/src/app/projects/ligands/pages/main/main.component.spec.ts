8b8099306301aebb0ff5c032c05962d2f9d02b03
roshan <roshan@ebi.ac.uk>
2024-04-22 14:38:45 +0100

    import http client for test of aggregated service api

--- apps/connect/src/app/projects/ligands/pages/main/main.component.spec.ts
+++ apps/connect/src/app/projects/ligands/pages/main/main.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { LigandsMainPageComponent } from './main.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LigandsMainPage', () => {
   let component: LigandsMainPageComponent;
@@ -8,7 +9,7 @@ describe('LigandsMainPage', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LigandsMainPageComponent, RouterTestingModule],
+      imports: [LigandsMainPageComponent, RouterTestingModule, HttpClientModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LigandsMainPageComponent);
--- apps/connect/src/app/projects/ligands/services/aggregated-api.service.spec.ts
+++ apps/connect/src/app/projects/ligands/services/aggregated-api.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AggregatedApiService } from './aggregated-api.service';
-
+import { HttpClientModule } from '@angular/common/http';
 describe('AggregatedApiService', () => {
   let service: AggregatedApiService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [AggregatedApiService],
+    }).compileComponents;
+
     service = TestBed.inject(AggregatedApiService);
   });
 
