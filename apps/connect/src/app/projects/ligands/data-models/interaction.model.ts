5800e078394f64fa82340a40d1ad1e734cf2e37f
roshan <roshan@ebi.ac.uk>
2024-05-31 09:51:51 +0100

    updated interaction model

--- apps/connect/src/app/projects/ligands/data-models/interaction.model.ts
+++ apps/connect/src/app/projects/ligands/data-models/interaction.model.ts
@@ -1,7 +1,18 @@
-export interface AtomResidueintx {
-  [key: string]: IntxType;
+export interface IntxType {
+  atom: string;
+  residue: string;
+  count: number;
 }
 
-export interface IntxType {
-  [key: string]: Array<[atom: string, residue: string, count: number]>;
+export interface IntxData {
+  [key: string]: IntxType[];
+}
+
+export interface PDBIntxData {
+  [key: string]: IntxData;
+}
+
+export interface IntxDataUrl {
+  IntxUrl: string;
+  interactions: PDBIntxData;
 }
