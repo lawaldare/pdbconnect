b879cbb278ad1ecaeae2cb1f0d94e3bc0eebccea
Sreenath Sasidharan Nair <sreenath@ebi.ac.uk>
2024-06-04 14:43:40 +0100

    Added tagging for docker images

--- bin/index.js
+++ bin/index.js
@@ -21,7 +21,7 @@ publish:${serviceName}:
         job: build
     script:
       - docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
-      - docker build --pull --cache-from $CI_REGISTRY_IMAGE --build-arg APP_BASE_HREF=\$${serviceName.toUpperCase()}_BASE_HREF --tag $CI_REGISTRY_IMAGE/connect-${serviceName} --file apps/${serviceName}/Dockerfile .
+      - docker build --pull --cache-from $CI_REGISTRY_IMAGE --build-arg APP_BASE_HREF=\$${serviceName.toUpperCase()}_BASE_HREF --tag $CI_REGISTRY_IMAGE/connect-${serviceName}:$CI_COMMIT_SHORT_SHA --file apps/${serviceName}/Dockerfile .
       - docker push $CI_REGISTRY_IMAGE/connect-${serviceName}
     tags:
       - pdbe-shell
