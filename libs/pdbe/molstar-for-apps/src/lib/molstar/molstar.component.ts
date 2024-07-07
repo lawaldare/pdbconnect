d8861f017804b470e930752980382aa5eb3c194d
Dare Lawal <lawal@ebi.ac.uk>
2024-07-07 09:05:18 +0100

    update

--- libs/pdbe/molstar-for-apps/src/lib/molstar/molstar.component.ts
+++ libs/pdbe/molstar-for-apps/src/lib/molstar/molstar.component.ts
@@ -15,13 +15,54 @@ export class MolstarComponent implements AfterViewInit {
   @Input() height!: string;
   @Input() width!: string;
 
+  private molstarViewInstance: any;
+
+  public isExpanded = false;
+
   @ViewChild('viewContainer') viewContainer!: ElementRef;
 
   ngAfterViewInit(): void {
-    const viewInstance = new PDBeMolstarPlugin();
+    this.molstarViewInstance = new PDBeMolstarPlugin();
 
     const container = this.viewContainer.nativeElement;
 
-    viewInstance.render(container, this.molstarConfig);
+    this.molstarViewInstance.render(container, this.molstarConfig);
+
+    this.molstarViewInstance.events.loadComplete.subscribe(() => {
+      const expandedLayout: HTMLDivElement | null = document.querySelector('.msp-plugin-content');
+
+      const observer = new MutationObserver((mutationsList) => {
+        const mutationRecord = mutationsList[0];
+        this.isExpanded = (mutationRecord.target as HTMLElement).classList.contains('msp-layout-expanded');
+        if (expandedLayout) {
+          this.updateMolstarView(this.isExpanded, expandedLayout);
+        }
+      });
+      if (expandedLayout) {
+        observer.observe(expandedLayout as Node, { attributes: true, attributeFilter: ['style', 'class'] });
+      }
+    });
+  }
+
+  private updateMolstarView(expanded: boolean, expandedLayout: HTMLDivElement | null) {
+    if (expandedLayout) {
+      if (expanded) {
+        expandedLayout.style.width = '95%';
+        expandedLayout.style.display = 'flex';
+        expandedLayout.style.justifyContent = 'center';
+        expandedLayout.style.height = '90%';
+        expandedLayout.style.margin = 'auto';
+      } else {
+        expandedLayout.style.width = '';
+        expandedLayout.style.display = '';
+        expandedLayout.style.justifyContent = '';
+        expandedLayout.style.height = '';
+        expandedLayout.style.top = '';
+      }
+    }
+  }
+
+  public triggerCollapse() {
+    this.molstarViewInstance.canvas.toggleExpanded(false);
   }
 }
