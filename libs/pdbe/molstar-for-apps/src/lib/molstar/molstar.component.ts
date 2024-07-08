56dea3c1c5c8b999710ba7bddd8a9c3bd6ae907d
Dare Lawal <lawal@ebi.ac.uk>
2024-07-08 07:46:20 +0100

    comment other implementation

--- libs/pdbe/molstar-for-apps/src/lib/molstar/molstar.component.ts
+++ libs/pdbe/molstar-for-apps/src/lib/molstar/molstar.component.ts
@@ -28,41 +28,41 @@ export class MolstarComponent implements AfterViewInit {
 
     this.molstarViewInstance.render(container, this.molstarConfig);
 
-    this.molstarViewInstance.events.loadComplete.subscribe(() => {
-      const expandedLayout: HTMLDivElement | null = document.querySelector('.msp-plugin-content');
+    // this.molstarViewInstance.events.loadComplete.subscribe(() => {
+    //   const expandedLayout: HTMLDivElement | null = document.querySelector('.msp-plugin-content');
 
-      const observer = new MutationObserver((mutationsList) => {
-        const mutationRecord = mutationsList[0];
-        this.isExpanded = (mutationRecord.target as HTMLElement).classList.contains('msp-layout-expanded');
-        if (expandedLayout) {
-          this.updateMolstarView(this.isExpanded, expandedLayout);
-        }
-      });
-      if (expandedLayout) {
-        observer.observe(expandedLayout as Node, { attributes: true, attributeFilter: ['style', 'class'] });
-      }
-    });
+    //   const observer = new MutationObserver((mutationsList) => {
+    //     const mutationRecord = mutationsList[0];
+    //     this.isExpanded = (mutationRecord.target as HTMLElement).classList.contains('msp-layout-expanded');
+    //     if (expandedLayout) {
+    //       this.updateMolstarView(this.isExpanded, expandedLayout);
+    //     }
+    //   });
+    //   if (expandedLayout) {
+    //     observer.observe(expandedLayout as Node, { attributes: true, attributeFilter: ['style', 'class'] });
+    //   }
+    // });
   }
 
-  private updateMolstarView(expanded: boolean, expandedLayout: HTMLDivElement | null) {
-    if (expandedLayout) {
-      if (expanded) {
-        expandedLayout.style.width = '95%';
-        expandedLayout.style.display = 'flex';
-        expandedLayout.style.justifyContent = 'center';
-        expandedLayout.style.height = '90%';
-        expandedLayout.style.margin = 'auto';
-      } else {
-        expandedLayout.style.width = '';
-        expandedLayout.style.display = '';
-        expandedLayout.style.justifyContent = '';
-        expandedLayout.style.height = '';
-        expandedLayout.style.top = '';
-      }
-    }
-  }
+  // private updateMolstarView(expanded: boolean, expandedLayout: HTMLDivElement | null) {
+  //   if (expandedLayout) {
+  //     if (expanded) {
+  //       expandedLayout.style.width = '95%';
+  //       expandedLayout.style.display = 'flex';
+  //       expandedLayout.style.justifyContent = 'center';
+  //       expandedLayout.style.height = '90%';
+  //       expandedLayout.style.margin = 'auto';
+  //     } else {
+  //       expandedLayout.style.width = '';
+  //       expandedLayout.style.display = '';
+  //       expandedLayout.style.justifyContent = '';
+  //       expandedLayout.style.height = '';
+  //       expandedLayout.style.top = '';
+  //     }
+  //   }
+  // }
 
-  public triggerCollapse() {
-    this.molstarViewInstance.canvas.toggleExpanded(false);
-  }
+  // public triggerCollapse() {
+  //   this.molstarViewInstance.canvas.toggleExpanded(false);
+  // }
 }
