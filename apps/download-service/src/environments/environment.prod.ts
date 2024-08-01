7469b2b99fb5c48a616ba9ddc82d4b90c36728be
Dare Lawal <lawal@ebi.ac.uk>
2024-08-01 16:09:46 +0100

    update

--- apps/download-service/src/environments/environment.prod.ts
+++ apps/download-service/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // downloadAPIUrl: 'https://www.ebi.ac.uk/pdbe/download/api/pdb',
-  downloadAPIUrl: 'https://wwwdev.ebi.ac.uk/pdbe/download/api/pdb',
+  downloadAPIUrl: 'https://www.ebi.ac.uk/pdbe/download/api/pdb',
 };
