97b6ae3735a84d6e10b71f809d2cfd99e0f840cb
Dare Lawal <lawal@ebi.ac.uk>
2024-09-03 13:15:17 +0100

    update

--- libs/pdbe/tool-tip/src/lib/tool-tip/tool-tip.component.ts
+++ libs/pdbe/tool-tip/src/lib/tool-tip/tool-tip.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,8 +9,8 @@ import { CommonModule } from '@angular/common';
   styleUrl: './tool-tip.component.scss',
 })
 export class ToolTipComponent {
-  @Input() minWidth = '323px';
-  @Input() textIcon = '';
+  public readonly minWidth = input<string>('323px');
+  public readonly textIcon = input<string>('');
   public readonly helpLogoSrc = '/assets/images/help_outline_24px.svg';
   public showTooltips = false;
 
