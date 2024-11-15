0e8b8d65e1bbee1a6603a310bbcd4ebbfc2eaeac
Sreenath Sasidharan Nair <sreenath@ebi.ac.uk>
2024-11-15 11:15:21 +0000

    Add conditional check for NX_BASE in CI configuration

--- apps/connect/src/environments/environment.prod.ts
+++ apps/connect/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   debug: false,
   pdbeBaseUrl: 'https://www.ebi.ac.uk/pdbe/',
-  googleAnalyticsTag: 'G-V1BCXVELRY',
+  googleAnalyticsTag: 'G-J9SJVN0SSD',
 };
