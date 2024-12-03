3177cb0c958d35fa8b680126d776e645240d46a9
Marcelo Afonso <marcelo@ebi.ac.uk>
2024-12-03 15:47:34 +0000

    migrated citation components

---
+++ apps/connect/src/app/projects/entry/components/citation-publication/citation-publication.component.ts
@@ -0,0 +1,42 @@
+import { ChangeDetectionStrategy, Component, input, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CitationArticleComponent } from '../citation-article/citation-article.component';
+import { Article } from '../../data-models/article.model';
+import { PageEvent } from '@angular/material/paginator';
+import { MaterialModule } from '@pdbc/core';
+
+@Component({
+  selector: 'pdbc-citation-publication',
+  standalone: true,
+  imports: [CommonModule, CitationArticleComponent, MaterialModule],
+  templateUrl: './citation-publication.component.html',
+  styleUrl: './citation-publication.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CitationPublicationComponent implements OnInit {
+  public publicationInfo = input.required<Article[]>();
+  public entryId = input.required<string>();
+  public headerText = input.required<string>();
+  public showFirstFive = signal(true);
+  public pagedPublicationInfo!: Article[];
+  public pageLength = signal(0);
+  public pageSize = signal(10);
+  public readonly pageSizeOptions = [5, 10, 15, 20];
+
+  ngOnInit(): void {
+    this.pageLength.set(this.publicationInfo().length);
+    this.pagedPublicationInfo = this.publicationInfo().slice(0, this.pageSize());
+  }
+
+  public showAllArticles(): void {
+    this.showFirstFive.update((value) => !value);
+  }
+
+  public handlePageEvent(event: PageEvent) {
+    const pageIndex = event.pageIndex;
+    this.pageSize.set(event.pageSize);
+    const startIndex = pageIndex * this.pageSize();
+    const endIndex = startIndex + this.pageSize();
+    this.pagedPublicationInfo = this.publicationInfo().slice(startIndex, endIndex);
+  }
+}
---
+++ apps/connect/src/app/projects/entry/components/citation-xml-images/citation-xml-images.component.ts
@@ -0,0 +1,64 @@
+import { AfterViewInit, Component, ElementRef, Inject, Renderer2, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { XMLImageRendererService, MaterialModule } from '@pdbc/core';
+import Splide from '@splidejs/splide';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'pdbc-citation-xml-images',
+  standalone: true,
+  imports: [CommonModule, MaterialModule],
+  templateUrl: './citation-xml-images.component.html',
+  styleUrl: './citation-xml-images.component.scss',
+})
+export class CitationXmlImagesComponent implements AfterViewInit {
+  @ViewChild('gallery', { read: ElementRef }) galleryContainer!: ElementRef;
+  @ViewChild('main', { read: ElementRef }) mainContainer!: ElementRef;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { entryId: string; imageXMLText: string },
+    private readonly xmlImageRendererService: XMLImageRendererService,
+    private readonly renderer: Renderer2,
+    private dialogRef: MatDialogRef<CitationXmlImagesComponent>
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.parseAndRenderXML();
+    setTimeout(() => {
+      const main = new Splide('#main', {
+        type: 'fade',
+        rewind: true,
+        pagination: false,
+        arrows: false,
+      });
+      const thumbnails = new Splide('#thumbnail', {
+        fixedWidth: 100,
+        fixedHeight: 60,
+        gap: 10,
+        rewind: true,
+        pagination: false,
+        isNavigation: true,
+        breakpoints: {
+          600: {
+            fixedWidth: 60,
+            fixedHeight: 44,
+          },
+        },
+      });
+      main.sync(thumbnails);
+      main.mount();
+      thumbnails.mount();
+    }, 500);
+  }
+
+  private parseAndRenderXML(): void {
+    const container = this.galleryContainer.nativeElement;
+    const main = this.mainContainer.nativeElement;
+    this.xmlImageRendererService.parseAndRenderXML(this.renderer, this.data.imageXMLText, container);
+    this.xmlImageRendererService.parseAndRenderXML(this.renderer, this.data.imageXMLText, main);
+  }
+
+  public closeDialog(): void {
+    this.dialogRef.close();
+  }
+}
