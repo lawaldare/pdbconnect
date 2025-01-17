f98d79b0a723ead77e7b08f1b0ec0ef611e866b4
lawal <lawal@ebi.ac.uk>
2025-01-17 16:53:26 +0000

    updated the query for the authors

--- apps/connect/src/app/projects/entry/directives/author-string.directive.ts
+++ apps/connect/src/app/projects/entry/directives/author-string.directive.ts
@@ -4,6 +4,7 @@
 /* eslint-disable no-prototype-builtins */
 
 import { Directive, Input, SimpleChanges, OnChanges, ElementRef, inject, Renderer2 } from '@angular/core';
+import { UtilService } from '@pdbc/core';
 
 @Directive({
   selector: '[authorsString]',
@@ -13,16 +14,17 @@ export class AuthorsStringDirective implements OnChanges {
   @Input() authors!: string;
   public readonly renderer = inject(Renderer2);
   public readonly el = inject(ElementRef);
+  private readonly util = inject(UtilService);
 
   ngOnChanges(changes: SimpleChanges) {
     const authors = changes['authors']?.currentValue;
 
-    const authorsArray = authors.split(',').map((author: string) => author.trim());
+    const authorsArray = authors.split('.,').map((author: string) => author.trim().replace(',', ''));
 
     for (const text of authorsArray) {
       const a = this.renderer.createElement('a');
-      a.textContent = authorsArray.indexOf(text) !== authorsArray.length - 1 ? `${text}, ` : `${text}.`;
-      a.href = `https://www.ebi.ac.uk/pdbe/entry/search/index?all_authors:${text}`;
+      a.textContent = authorsArray.indexOf(text) !== authorsArray.length - 1 ? `${text}, ` : `${text}`;
+      a.href = this.util.generateSortedQueryURL(text, 'q_entry_authors');
       this.renderer.setAttribute(a, 'target', '_blank');
       this.renderer.appendChild(this.el.nativeElement, a);
       this.renderer.setAttribute(a, 'style', 'margin-right:5px;');
