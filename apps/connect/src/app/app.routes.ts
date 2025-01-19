05efe534da06e08f49715f7551d56172545cb27f
lawal <lawal@ebi.ac.uk>
2025-01-19 22:09:01 +0000

    lazy loaded the routes

--- apps/connect/src/app/app.routes.ts
+++ apps/connect/src/app/app.routes.ts
@@ -1,17 +1,25 @@
 import { Route } from '@angular/router';
-import { EntryMainPageComponent } from './projects/entry/pages/main/main.component';
-import { ProteinsMainPageComponent } from './projects/proteins/pages/main/main.component';
-import { LigandWrapperComponent } from './projects/ligands/components/pages/ligand-wrapper/ligand-wrapper.component';
-import { MainComponent } from './projects/complex/components/pages/main/main.component';
-import { LigandHomepageComponent } from './projects/ligands/components/pages/ligand-homepage/ligand-homepage.component';
-import { LatestReleasesComponent } from './projects/ligands/components/pages/latest-releases/latest-releases.component';
+// import { LigandHomepageComponent } from './projects/ligands/components/pages/ligand-homepage/ligand-homepage.component';
+// import { LatestReleasesComponent } from './projects/ligands/components/pages/latest-releases/latest-releases.component';
 
 export const appRoutes: Route[] = [
-  { path: '', component: LigandHomepageComponent, title: 'PDBe Knowledge base' },
-  { path: 'latest-releases', component: LatestReleasesComponent, title: 'PDBe Knowledge base' },
-  { path: 'entry/pdb/:entryId', component: EntryMainPageComponent, title: 'PDBe Entry Pages' },
-  { path: 'proteins/:entryId', component: ProteinsMainPageComponent, title: 'PDBe Proteins Pages' },
-  { path: 'complex/:complexId', component: MainComponent, title: 'Complex Pages' },
-  { path: 'chemicalCompound/show/:ligandId', component: LigandWrapperComponent, title: 'PDBe Ligand Pages (PDBeChem)' },
+  // { path: '', component: LigandHomepageComponent, title: 'PDBe Knowledge base' },
+  // { path: 'latest-releases', component: LatestReleasesComponent, title: 'PDBe Knowledge base' },
+  {
+    path: 'entry/pdb/:entryId',
+    loadComponent: () => import('./projects/entry/pages/main/main.component').then((m) => m.EntryMainPageComponent),
+    title: 'PDBe Entry Pages',
+  },
+  // { path: 'proteins/:entryId', component: ProteinsMainPageComponent, title: 'PDBe Proteins Pages' },
+  {
+    path: 'complex/:complexId',
+    loadComponent: () => import('./projects/complex/components/pages/main/main.component').then((m) => m.MainComponent),
+    title: 'Complex Pages',
+  },
+  {
+    path: 'chemicalCompound/show/:ligandId',
+    loadComponent: () => import('./projects/ligands/components/pages/ligand-wrapper/ligand-wrapper.component').then((m) => m.LigandWrapperComponent),
+    title: 'PDBe Ligand Pages (PDBeChem)',
+  },
   // { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
