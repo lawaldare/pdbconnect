111e760e9b86244d5c48fb2289dfb9b943de1543
Marcelo Afonso <marcelo@ebi.ac.uk>
2025-04-04 15:12:29 +0100

    PDBE-7467: created protvista service

---
+++ apps/connect/src/app/projects/entry/services/entry-pv-nightingale-api.service.ts
@@ -0,0 +1,36 @@
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { APIConservationData, APITrackData, APIVariationData } from '@pdbe-lib/pv-nightingale-components';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PvDataApiService {
+  private readonly http = inject(HttpClient);
+
+  /**
+   * Endpoints list for entryId + entityId
+   * https://www${appUrlEnv}.ebi.ac.uk/pdbe/graph-api/pdbe_pages/protvista/uniprot_mapping/${entryId}/${entityId}
+   * https://www${appUrlEnv}.ebi.ac.uk/pdbe/graph-api/pdbe_pages/protvista/chains/${entryId}/${entityId}
+   * https://www${appUrlEnv}.ebi.ac.uk/pdbe/graph-api/pdbe_pages/protvista/domains/${entryId}/${entityId}
+   * https://www${appUrlEnv}.ebi.ac.uk/pdbe/graph-api/pdbe_pages/protvista/rfam/${entryId}/${entityId}
+   * https://www${appUrlEnv}.ebi.ac.uk/pdbe/graph-api/pdbe_pages/protvista/secondary_structure/${entryId}/${entityId}
+   * https://www${appUrlEnv}.ebi.ac.uk/pdbe/graph-api/pdbe_pages/protvista/binding_sites/${entryId}/${entityId}
+   * https://www${appUrlEnv}.ebi.ac.uk/pdbe/graph-api/pdbe_pages/protvista/interfaces/${entryId}/${entityId}
+   * https://www${appUrlEnv}.ebi.ac.uk/pdbe/graph-api/pdbe_pages/protvista/annotations/${entryId}/${entityId}
+   */
+  public getPdbeEntityTrackData(entryId: string, entityId: string, endpoint: string): Observable<Record<string, APITrackData>> {
+    const url = `${environment.pdbeBaseUrl}}graph-api/pdbe_pages/protvista/${endpoint}/${entryId}/${entityId}`;
+    return this.http.get<Record<string, APITrackData>>(url);
+  }
+  public getPdbeConservationTrackData(entryId: string, entityId: string) {
+    const url = `${environment.pdbeBaseUrl}}graph-api/pdb/sequence_conservation/${entryId}/${entityId}`;
+    return this.http.get<APIConservationData>(url);
+  }
+  public getPdbeVariationTrackData(entryId: string, entityId: string) {
+    const url = `${environment.pdbeBaseUrl}}graph-api/pdbe_pages/protvista/variation/${entryId}/${entityId}`;
+    return this.http.get<APIVariationData>(url);
+  }
+}
--- libs/pdbe/pv-nightingale-components/src/index.ts
+++ libs/pdbe/pv-nightingale-components/src/index.ts
@@ -5,4 +5,7 @@ export { VariationTrackBlockComponent } from './lib/pv-nightingale-components/co
 export { TrackBlockComponent } from './lib/pv-nightingale-components/components/expandable-tracks/pv-track-block/pv-track-block.component';
 export { PvTooltipService } from './lib/pv-nightingale-components/services/pv-tooltip.service';
 export { PvFixedHighlightService } from './lib/pv-nightingale-components/services/pv-fixed-highlight.service';
+export { APITrackData, APITrackDatum, APITrackItem, APITrackFragment } from './lib/pv-nightingale-components/models/pv-api-general-track-data.model';
+export { APIConservationData } from './lib/pv-nightingale-components/models/pv-api-conservation-track-data.model';
+export { APIVariationData } from './lib/pv-nightingale-components/models/pv-api-variation-track-data.model';
 export * from './lib/pv-nightingale-components/pv-nightingale-components.component';
