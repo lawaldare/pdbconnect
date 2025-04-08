afece485a96052f804031e9bc47bd89447634337
Marcelo Afonso <marcelo@ebi.ac.uk>
2025-04-08 15:12:46 +0100

    PDBE-7467: fixed API endpoint strings

--- apps/connect/src/app/projects/entry/services/entry-pv-nightingale-api.service.ts
+++ apps/connect/src/app/projects/entry/services/entry-pv-nightingale-api.service.ts
@@ -1,4 +1,4 @@
-import { computed, inject, Injectable, signal } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { APIConservationData, APITrackData, APIVariationData } from '@pdbe-lib/pv-nightingale-components';
@@ -22,15 +22,15 @@ export class PvDataApiService {
    * https://www${appUrlEnv}.ebi.ac.uk/pdbe/graph-api/pdbe_pages/protvista/annotations/${entryId}/${entityId}
    */
   public getPdbeEntityTrackData(entryId: string, entityId: string, endpoint: string): Observable<Record<string, APITrackData>> {
-    const url = `${environment.pdbeBaseUrl}}graph-api/pdbe_pages/protvista/${endpoint}/${entryId}/${entityId}`;
+    const url = `${environment.pdbeBaseUrl}graph-api/pdbe_pages/protvista/${endpoint}/${entryId}/${entityId}`;
     return this.http.get<Record<string, APITrackData>>(url);
   }
   public getPdbeConservationTrackData(entryId: string, entityId: string) {
-    const url = `${environment.pdbeBaseUrl}}graph-api/pdb/sequence_conservation/${entryId}/${entityId}`;
+    const url = `${environment.pdbeBaseUrl}graph-api/pdb/sequence_conservation/${entryId}/${entityId}`;
     return this.http.get<APIConservationData>(url);
   }
   public getPdbeVariationTrackData(entryId: string, entityId: string) {
-    const url = `${environment.pdbeBaseUrl}}graph-api/pdbe_pages/protvista/variation/${entryId}/${entityId}`;
+    const url = `${environment.pdbeBaseUrl}graph-api/pdbe_pages/protvista/variation/${entryId}/${entityId}`;
     return this.http.get<APIVariationData>(url);
   }
 }
