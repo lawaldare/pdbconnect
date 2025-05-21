fe472487d6bd90b6216437ee42892e17ecf10d4c
Sreenath Sasidharan Nair <sreenath@ebi.ac.uk>
2025-05-21 16:35:04 +0100

    Test out webpack publicPath tweak for serving assets via different path

--- apps/connect/src/main.ts
+++ apps/connect/src/main.ts
@@ -1,3 +1,6 @@
+declare let __webpack_public_path__: string;
+__webpack_public_path__ = (window as any).__app_public_path__ || '/';
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
