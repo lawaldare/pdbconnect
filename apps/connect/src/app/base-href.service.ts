97b196b8e23ce7aa4a0737f27564ac5430823025
lawal <lawal@ebi.ac.uk>
2025-05-21 11:37:59 +0100

    reverted back

--- apps/connect/src/app/base-href.service.ts
+++ apps/connect/src/app/base-href.service.ts
@@ -7,18 +7,13 @@ export class BaseHrefService {
   public setBaseHref() {
     const hostname = document.location.hostname;
     const pathname = document.location.pathname;
-    const pathnamesArray = pathname.split('/');
-    const mappedBaseHref = `/${pathnamesArray[1]}/${pathnamesArray[2]}/`;
     let baseHref: string;
-
-    console.log('pathname:->', pathname);
 
     if (hostname === 'localhost') {
       baseHref = '/';
-    } else if (mappedBaseHref === '/pdbe-srv/pdbechem/') {
-      baseHref = '/pdbe/connect/';
     } else {
-      baseHref = mappedBaseHref;
+      const pathnamesArray = pathname.split('/');
+      baseHref = `/${pathnamesArray[1]}/${pathnamesArray[2]}/`;
     }
 
     const baseTag = document.querySelector('base');
