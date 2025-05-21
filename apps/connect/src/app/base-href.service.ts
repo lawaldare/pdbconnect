e335e7a9ec38d88659664007817bf2d1b7966398
lawal <lawal@ebi.ac.uk>
2025-05-21 11:11:24 +0100

    updated the baseHref for Ligand pages

--- apps/connect/src/app/base-href.service.ts
+++ apps/connect/src/app/base-href.service.ts
@@ -7,17 +7,18 @@ export class BaseHrefService {
   public setBaseHref() {
     const hostname = document.location.hostname;
     const pathname = document.location.pathname;
+    const pathnamesArray = pathname.split('/');
+    const mappedBaseHref = `/${pathnamesArray[1]}/${pathnamesArray[2]}/`;
     let baseHref: string;
 
     console.log('pathname:->', pathname);
 
     if (hostname === 'localhost') {
       baseHref = '/';
-    } else if (pathname === '/pdbe-srv/pdbechem/') {
+    } else if (mappedBaseHref === '/pdbe-srv/pdbechem/') {
       baseHref = '/pdbe/connect/';
     } else {
-      const pathnamesArray = pathname.split('/');
-      baseHref = `/${pathnamesArray[1]}/${pathnamesArray[2]}/`;
+      baseHref = mappedBaseHref;
     }
 
     const baseTag = document.querySelector('base');
