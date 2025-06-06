0657105cec435a6dec322b9a4ba19db44a01c34a
lawal <lawal@ebi.ac.uk>
2025-06-06 11:29:04 +0100

    updated the routes

--- apps/connect/src/app/app.routes.ts
+++ apps/connect/src/app/app.routes.ts
@@ -14,12 +14,13 @@ export const appRoutes: Route[] = [
     loadComponent: () => import('./projects/ligands/components/pages/ligand-wrapper/ligand-wrapper.component').then((m) => m.LigandWrapperComponent),
     title: 'PDBe-KB Ligand Pages (PDBeChem)',
   },
-  { path: 'error', component: ErrorPageComponent, title: '404 Not Found' },
   {
     path: environment.isLocal ? ':complexId' : 'complex/:complexId',
     loadComponent: () => import('./projects/complex/components/pages/main/main.component').then((m) => m.MainComponent),
     title: 'Complex Pages',
     canActivate: [complexIdGuard],
   },
-  { path: '**', redirectTo: 'error' },
+  { path: 'error', component: ErrorPageComponent, title: '404 Not Found' },
+
+  // { path: '**', redirectTo: 'error' },
 ];
