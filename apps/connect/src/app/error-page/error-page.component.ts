bfbdb5bc7752effaf32131c2186e6192474ae742
lawal <lawal@ebi.ac.uk>
2025-06-06 10:56:00 +0100

    updated the error page

--- apps/connect/src/app/error-page/error-page.component.ts
+++ apps/connect/src/app/error-page/error-page.component.ts
@@ -20,14 +20,14 @@ export interface Error {
 export class ErrorPageComponent {
   public readonly headerLogoMenuConfig = pdbeLogoConfig;
   private readonly route = inject(ActivatedRoute);
-  public statusCode = signal<string>('404');
+  public statusCode = signal<number>(404);
   private errorsList: Record<string, Error> = ErrorsList;
   public error = signal<Error>({} as Error);
 
   constructor() {
     this.route.queryParams.subscribe((params) => {
-      const code = params['status'] ?? this.statusCode();
-      this.statusCode.set(code);
+      const code = params['status'] ?? String(this.statusCode());
+      this.statusCode.set(Number(code));
       const error = this.errorsList[code];
       this.error.set(error);
     });
