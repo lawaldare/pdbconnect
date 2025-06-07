32bc251249000d7cd1e10f055118a3add97b8b37
lawal <lawal@ebi.ac.uk>
2025-06-07 06:04:08 +0100

    updates

--- apps/connect/src/app/projects/entry/pages/main/main.component.ts
+++ apps/connect/src/app/projects/entry/pages/main/main.component.ts
@@ -35,7 +35,6 @@ import { ActionQueueService } from '../../services/action-queue.service';
 import { MolstarStateService } from '../../services/molstar-state.service';
 import Clarity from '@microsoft/clarity';
 import { NotificationComponent } from '@pdbc/notification';
-import { Actions, ofType } from '@ngrx/effects';
 
 export type TableNames = 'Assemblies' | 'Macromolecules' | 'Ligands' | 'Domains';
 
@@ -81,7 +80,6 @@ export type TableNames = 'Assemblies' | 'Macromolecules' | 'Ligands' | 'Domains'
 })
 export class EntryMainPageComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
-  private readonly actions$ = inject(Actions);
   public readonly dataProcessing = inject(MainDataProcessingFacade);
   private readonly destroyRef = inject(DestroyRef);
   private readonly globalStore = inject(Store<EntryStoreState>);
@@ -140,19 +138,6 @@ export class EntryMainPageComponent implements OnInit {
       const tabIndex = routeTabs.findIndex((tab) => tab.id === tabName);
       this.selectedTab.set(tabIndex);
     });
-
-    // this.actions$
-    //   .pipe(
-    //     ofType(EntryActions.getEntryStatusFailure),
-    //     tap(({ error }) => {
-    //       this.router.navigate(['/error'], {
-    //         queryParams: { status: (error as any)?.status },
-    //         queryParamsHandling: 'merge',
-    //       });
-    //     }),
-    //     takeUntilDestroyed(this.destroyRef)
-    //   )
-    //   .subscribe();
 
     effect(async () => {
       // this effect runs only once because of molstarFirstRenderStarted
