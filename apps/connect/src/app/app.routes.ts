bf03ec15752100714ae193775de660330aa9f2aa
lawal <lawal@ebi.ac.uk>
2025-06-11 07:18:16 +0100

    updates

--- apps/connect/src/app/app.routes.ts
+++ apps/connect/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Route } from '@angular/router';
-import { environment } from '../environments/environment';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { complexIdGuard } from './projects/complex/guards/complex.guard';
 
-console.log('isLocal', environment.isLocal);
+const hostname = document.location.hostname;
+const isLocal = hostname === 'localhost';
+
+console.log('isLocal', isLocal);
 
 export const appRoutes: Route[] = [
   {
-    path: environment.isLocal ? 'pdb/:entryId' : 'entry/pdb/:entryId',
+    path: isLocal ? 'pdb/:entryId' : 'entry/pdb/:entryId',
     loadComponent: () => import('./projects/entry/pages/main/main.component').then((m) => m.EntryMainPageComponent),
     title: 'PDBe Entry Pages',
   },
@@ -18,7 +20,7 @@ export const appRoutes: Route[] = [
   },
   { path: 'error', component: ErrorPageComponent, title: 'Error Page' },
   {
-    path: environment.isLocal ? ':complexId' : 'complexes/:complexId',
+    path: isLocal ? ':complexId' : 'complexes/:complexId',
     loadComponent: () => import('./projects/complex/components/pages/main/main.component').then((m) => m.MainComponent),
     title: 'Complex Pages',
     canActivate: [complexIdGuard],
