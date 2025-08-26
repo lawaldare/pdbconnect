0cc03e67337767f738fc0ca3c3f01b20a079e170
Marcelo Afonso <marcelo@ebi.ac.uk>
2025-08-26 11:29:09 +0100

    revert 4b018cd0 keeping prev error handling

--- apps/connect/src/app/projects/entry/pages/main/main.component.ts
+++ apps/connect/src/app/projects/entry/pages/main/main.component.ts
@@ -35,7 +35,7 @@ import { EntryUtilService } from '../../services/entry-util.service';
 import { LLMTabComponent } from '../../components/llm-tab/llm-tab.component';
 import { VisualisationInteractivityDirective } from '../../directives/visualisation-interactivity.directive';
 import { EntryBioschemasService } from '../../services/entry.bioschemas';
-// import { ErrorPageComponent } from '../../../../error-page/error-page.component';
+import { ErrorPageComponent } from '../../../../error-page/error-page.component';
 
 // Some interesting entries:
 // 4aqd carbs
@@ -68,7 +68,7 @@ import { EntryBioschemasService } from '../../services/entry.bioschemas';
     LigandsTabComponent,
     DomainsTabComponent,
     NotificationComponent,
-    // ErrorPageComponent,
+    ErrorPageComponent,
     VisualisationInteractivityDirective,
   ],
   templateUrl: './main.component.html',
