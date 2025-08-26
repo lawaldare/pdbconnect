921552c5411dc7bc70c2d5089be4cd785739c7d7
Marcelo Afonso <marcelo@ebi.ac.uk>
2025-08-26 16:45:07 +0100

    aggregated api for protvista in dev and prod

--- apps/connect/src/app/projects/entry/services/entry-pv-nightingale-api.service.ts
+++ apps/connect/src/app/projects/entry/services/entry-pv-nightingale-api.service.ts
@@ -9,7 +9,30 @@ import { environment } from '../../../../environments/environment';
 })
 export class PvDataApiService {
   private readonly http = inject(HttpClient);
+  private readonly isLocalhost = window?.location?.hostname === 'localhost';
+
   private readonly BaseAPI = `https://www.ebi.ac.uk/pdbe/graph-api/`;
+  private readonly AggregatedApiUrl = `${environment.pdbeBaseUrl}api/v2/`;
+
+  private buildUrl(endpoint: string, entryId: string, entityId: string): string {
+    const baseUrl = this.isLocalhost ? this.BaseAPI : this.AggregatedApiUrl;
+    let urlPath = this.isLocalhost ? 'pdbe_pages/protvista/' : 'pdb/entry/protvista/';
+    if (endpoint === 'sequence_conservation') urlPath = 'pdb/';
+    return `${baseUrl}${urlPath}${endpoint}/${entryId}/${entityId}`;
+  }
+
+  private handleTrackDataResponse(entryId: string) {
+    return (source: Observable<Record<string, APITrackData>>) =>
+      source.pipe(
+        map((data) => data[entryId]),
+        catchError((error) => {
+          if (error?.status === 404) {
+            return of({ empty: true } as unknown as APITrackData);
+          }
+          return throwError(() => error);
+        })
+      );
+  }
 
   /**
    * Endpoints list for entryId + entityId
@@ -32,116 +55,55 @@ export class PvDataApiService {
    */
 
   public getPdbeEntityUniprotMappingTrackData(entryId: string, entityId: string): Observable<APITrackData> {
-    const url = `${this.BaseAPI}pdbe_pages/protvista/uniprot_mapping/${entryId}/${entityId}`;
-    return this.http.get<Record<string, APITrackData>>(url).pipe(
-      map((data) => data[entryId]),
-      catchError((error) => {
-        if (error?.status === 404) {
-          return of({ empty: true } as unknown as APITrackData);
-        }
-        return throwError(() => error); // rethrow for anything else
-      })
-    );
+    const url = this.buildUrl('uniprot_mapping', entryId, entityId);
+    console.log('url');
+    console.log(url);
+    return this.http.get<Record<string, APITrackData>>(url).pipe(this.handleTrackDataResponse(entryId));
   }
 
   public getPdbeEntityChainsTrackData(entryId: string, entityId: string): Observable<APITrackData> {
-    const url = `${this.BaseAPI}pdbe_pages/protvista/chains/${entryId}/${entityId}`;
-    return this.http.get<Record<string, APITrackData>>(url).pipe(
-      map((data) => data[entryId]),
-      catchError((error) => {
-        if (error?.status === 404) {
-          return of({ empty: true } as unknown as APITrackData);
-        }
-        return throwError(() => error); // rethrow for anything else
-      })
-    );
+    const url = this.buildUrl('chains', entryId, entityId);
+    return this.http.get<Record<string, APITrackData>>(url).pipe(this.handleTrackDataResponse(entryId));
   }
 
   public getPdbeEntityDomainsTrackData(entryId: string, entityId: string): Observable<APITrackData> {
-    const url = `${this.BaseAPI}pdbe_pages/protvista/domains/${entryId}/${entityId}`;
-    return this.http.get<Record<string, APITrackData>>(url).pipe(
-      map((data) => data[entryId]),
-      catchError((error) => {
-        if (error?.status === 404) {
-          return of({ empty: true } as unknown as APITrackData);
-        }
-        return throwError(() => error); // rethrow for anything else
-      })
-    );
+    const url = this.buildUrl('domains', entryId, entityId);
+    return this.http.get<Record<string, APITrackData>>(url).pipe(this.handleTrackDataResponse(entryId));
   }
 
   public getPdbeEntityRfamTrackData(entryId: string, entityId: string): Observable<APITrackData> {
-    const url = `${this.BaseAPI}pdbe_pages/protvista/rfam/${entryId}/${entityId}`;
-    return this.http.get<Record<string, APITrackData>>(url).pipe(
-      map((data) => data[entryId]),
-      catchError((error) => {
-        if (error?.status === 404) {
-          return of({ empty: true } as unknown as APITrackData);
-        }
-        return throwError(() => error); // rethrow for anything else
-      })
-    );
+    const url = this.buildUrl('rfam', entryId, entityId);
+    return this.http.get<Record<string, APITrackData>>(url).pipe(this.handleTrackDataResponse(entryId));
   }
 
   public getPdbeEntitySecondaryStructureTrackData(entryId: string, entityId: string): Observable<APITrackData> {
-    const url = `${this.BaseAPI}pdbe_pages/protvista/secondary_structure/${entryId}/${entityId}`;
-    return this.http.get<Record<string, APITrackData>>(url).pipe(
-      map((data) => data[entryId]),
-      catchError((error) => {
-        if (error?.status === 404) {
-          return of({ empty: true } as unknown as APITrackData);
-        }
-        return throwError(() => error); // rethrow for anything else
-      })
-    );
+    const url = this.buildUrl('secondary_structure', entryId, entityId);
+    return this.http.get<Record<string, APITrackData>>(url).pipe(this.handleTrackDataResponse(entryId));
   }
 
   public getPdbeEntityBindingSitesTrackData(entryId: string, entityId: string): Observable<APITrackData> {
-    const url = `${this.BaseAPI}pdbe_pages/protvista/binding_sites/${entryId}/${entityId}`;
-    return this.http.get<Record<string, APITrackData>>(url).pipe(
-      map((data) => data[entryId]),
-      catchError((error) => {
-        if (error?.status === 404) {
-          return of({ empty: true } as unknown as APITrackData);
-        }
-        return throwError(() => error); // rethrow for anything else
-      })
-    );
+    const url = this.buildUrl('binding_sites', entryId, entityId);
+    return this.http.get<Record<string, APITrackData>>(url).pipe(this.handleTrackDataResponse(entryId));
   }
 
   public getPdbeEntityInterfacesTrackData(entryId: string, entityId: string): Observable<APITrackData> {
-    const url = `${this.BaseAPI}pdbe_pages/protvista/interfaces/${entryId}/${entityId}`;
-    return this.http.get<Record<string, APITrackData>>(url).pipe(
-      map((data) => data[entryId]),
-      catchError((error) => {
-        if (error?.status === 404) {
-          return of({ empty: true } as unknown as APITrackData);
-        }
-        return throwError(() => error); // rethrow for anything else
-      })
-    );
+    const url = this.buildUrl('interfaces', entryId, entityId);
+    return this.http.get<Record<string, APITrackData>>(url).pipe(this.handleTrackDataResponse(entryId));
   }
 
   public getPdbeEntityAnnotationsTrackData(entryId: string, entityId: string): Observable<APITrackData> {
-    const url = `${this.BaseAPI}pdbe_pages/protvista/annotations/${entryId}/${entityId}`;
-    return this.http.get<Record<string, APITrackData>>(url).pipe(
-      map((data) => data[entryId]),
-      catchError((error) => {
-        if (error?.status === 404) {
-          return of({ empty: true } as unknown as APITrackData);
-        }
-        return throwError(() => error); // rethrow for anything else
-      })
-    );
+    const url = this.buildUrl('annotations', entryId, entityId);
+    return this.http.get<Record<string, APITrackData>>(url).pipe(this.handleTrackDataResponse(entryId));
   }
 
   public getPdbeConservationTrackData(entryId: string, entityId: string) {
-    const url = `${this.BaseAPI}pdb/sequence_conservation/${entryId}/${entityId}`;
+    const url = this.buildUrl('sequence_conservation', entryId, entityId);
     return this.http.get<APIConservationData>(url);
   }
 
   public getPdbeVariationTrackData(entryId: string, entityId: string) {
     const url = `${this.BaseAPI}pdbe_pages/protvista/variation/${entryId}/${entityId}`;
+    // const url = this.buildUrl('variation', entryId, entityId);
     return this.http.get<APIVariationData>(url);
   }
 }
