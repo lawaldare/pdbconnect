8957fd77690143caa8e60e040836c79885573aec
Marcelo Afonso <marcelo@ebi.ac.uk>
2025-09-30 14:00:59 +0100

    fixed og:url and twitter:url

--- apps/connect/src/app/projects/entry/services/meta-tag.service.ts
+++ apps/connect/src/app/projects/entry/services/meta-tag.service.ts
@@ -40,7 +40,7 @@ export class MetaTagService {
 
             this.metaService.addTag({ property: 'og:title', content: `PDB: ${this.entryId()} | Protein Data Bank in Europe - PDBe` });
             this.metaService.addTag({ property: 'og:description', content: `Entry title: "${summaryData.entryTitle}"` });
-            this.metaService.addTag({ property: 'og:url', content: `${environment.baseUrl}pdbe//entry/pdb/1trn` });
+            this.metaService.addTag({ property: 'og:url', content: `${environment.baseUrl}pdbe/entry/pdb/${this.entryId()}` });
             this.metaService.addTag({
               property: 'og:image',
               content: `https://www.ebi.ac.uk/pdbe/static/entry/${this.entryId()}_deposited_chain_front_image-800x800.png`,
@@ -53,7 +53,7 @@ export class MetaTagService {
             this.metaService.addTag({ name: 'twitter:card', content: 'summary_large_image' });
             this.metaService.addTag({ name: 'twitter:title', content: titleAndDescription });
             this.metaService.addTag({ name: 'twitter:description', content: titleAndDescription });
-            this.metaService.addTag({ name: 'twitter:url', content: `${environment.baseUrl}pdbe//entry/pdb/1trn` });
+            this.metaService.addTag({ name: 'twitter:url', content: `${environment.baseUrl}pdbe/entry/pdb/${this.entryId()}` });
             this.metaService.addTag({
               name: 'twitter:image',
               content: `https://www.ebi.ac.uk/pdbe/static/entry/${this.entryId()}_deposited_chain_front_image-800x800.png`,
