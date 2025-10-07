ec4fb8ffff8a33668159339745f7361fff039dd3
lawal <lawal@ebi.ac.uk>
2025-10-07 12:05:14 +0100

    updates

--- apps/connect/src/app/projects/complex/components/pages/main/main.component.ts
+++ apps/connect/src/app/projects/complex/components/pages/main/main.component.ts
@@ -2,7 +2,7 @@ import { Component, DestroyRef, inject, OnInit, Renderer2, signal, ViewChild } f
 import { CommonModule } from '@angular/common';
 
 import { PdbeHeaderLogoMenuComponent } from '@pdbe-lib/header-logo-menu';
-import { PdbeHeaderSearchComponent } from '@pdbe-lib/header-search';
+// import { PdbeHeaderSearchComponent } from '@pdbe-lib/header-search';
 import { SummaryComponent } from '../../page-sections/summary/summary.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { of, switchMap } from 'rxjs';
@@ -34,7 +34,7 @@ import { DataPrivacyBannerComponent } from '@pdbc/core';
   imports: [
     CommonModule,
     PdbeHeaderLogoMenuComponent,
-    PdbeHeaderSearchComponent,
+    // PdbeHeaderSearchComponent,
     SummaryComponent,
     ComplexStructuresComponent,
     ComplexPISAComponent,
@@ -60,7 +60,7 @@ export class MainComponent implements OnInit {
   private readonly bioschemasService = inject(ComplexBioschemasService);
   private readonly renderer = inject(Renderer2);
 
-  public readonly headerLogoMenuConfig = headerComplexLogoMenuConfig;
+  public readonly headerLogoMenuConfig = { ...headerComplexLogoMenuConfig, isComplexPage: true };
   public readonly headerSearchConfig = headerSearchComplexConfig;
 
   private readonly globalStore = inject(Store<ComplexStoreState>);
