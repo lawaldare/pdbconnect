1c65e2cf6bf4dc545a2cbd92d95ccf9aaaf7cd08
lawal <lawal@ebi.ac.uk>
2025-10-23 06:47:36 +0100

    updated the example

--- apps/connect/src/app/projects/complex/complex.constant.ts
+++ apps/connect/src/app/projects/complex/complex.constant.ts
@@ -4,8 +4,8 @@ import * as d3 from 'd3';
 export const headerSearchComplexConfig = {
   examples: [
     {
-      label: 'Hemoglobin HbA (PDB-CPX-154652)',
-      value: 'PDB-CPX-154652',
+      label: 'Hemoglobin HbA (PDB-CPX-143959)',
+      value: 'PDB-CPX-143959',
     },
     {
       label: 'Cyclin A2-CDK2 (PDB-CPX-148886)',
