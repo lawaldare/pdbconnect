2ce531697f0a66a13487b3d45e7c749d156b0c0a
lawal <lawal@ebi.ac.uk>
2025-10-30 10:27:16 +0000

    setting up...

---
+++ apps/pisa/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  debug: false,
+  baseUrl: 'https://www.ebi.ac.uk/',
+  googleAnalyticsTag: 'G-J9SJVN0SSD',
+  clarityProjectIdForEntryPages: 'tctdey0l2n',
+  clarityProjectIdForLigandPages: 'tx72ecxs08',
+  clarityProjectIdForComplexPages: 'tx74d6kier',
+};
