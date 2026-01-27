33b3915b6fd08880e2a8484dd42b35cf5f3ade56
lawal <lawal@ebi.ac.uk>
2026-01-27 11:28:54 +0000

    PISA APP: added json download faeture

--- apps/pisa/src/app/components/single-interface-details/single-interface-details.ts
+++ apps/pisa/src/app/components/single-interface-details/single-interface-details.ts
@@ -26,63 +26,83 @@ export class SingleInterfaceDetailsComponent {
       .select(PisaSelectors.interfaceResultForInterfaceIdInterfacesTab)
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe((interfaceResult) => {
-        console.log('Downloading interface result:', interfaceResult);
         const structure1 = interfaceResult.interface.molecules[0];
         const structure2 = interfaceResult.interface.molecules[1];
+
+        const authAsymIdForStructure1 = interfaceResult.interface.molecules[0].auth_asym_id;
+        const authAsymIdForStructure2 = interfaceResult.interface.molecules[1].auth_asym_id;
+
+        const residuesForStructure1 = interfaceResult.interface.molecules[0]?.residues?.residues || [];
+        const residuesForStructure2 = interfaceResult.interface.molecules[1]?.residues?.residues || [];
+
+        const structure1Data = residuesForStructure1.map((residue: any) => ({ ...residue, auth_sym_id: authAsymIdForStructure1 }));
+        const structure2Data = residuesForStructure2.map((residue: any) => ({ ...residue, auth_sym_id: authAsymIdForStructure2 }));
         const interfaceJSON: any = {
-          structure_1: {
-            symmetry_operation: structure1.symmetry_operation,
-            symmetry_id: structure1.symmetry_id,
-            atoms: {
-              interface: structure1.int_natoms,
-              surface: structure1.extendedData.n_surface_atoms,
-              total: structure1.extendedData.total_atoms,
+          interface: {
+            structure_1: {
+              symmetry_operation: structure1.symmetry_operation,
+              symmetry_id: structure1.symmetry_id,
+              atoms: {
+                interface: structure1.int_natoms,
+                surface: structure1.extendedData.n_surface_atoms,
+                total: structure1.extendedData.total_atoms,
+              },
+              residues: {
+                interface: structure1.int_nres,
+                surface: structure1.extendedData.n_surface_residues,
+                total: structure1.extendedData.total_residues,
+              },
+              solvent_accessible_area: {
+                interface: structure1.int_area,
+                total: structure1.extendedData.asa,
+              },
+              solvation_energy: {
+                isolated_structure: structure1.extendedData.solv_energy,
+                gain_at_complexation: -6.3,
+                p_value: structure1.pvalue,
+              },
             },
-            residues: {
-              interface: structure1.int_nres,
-              surface: structure1.extendedData.n_surface_residues,
-              total: structure1.extendedData.total_residues,
-            },
-            solvent_accessible_area: {
-              interface: structure1.int_area,
-              total: structure1.extendedData.asa,
-            },
-            solvation_energy: {
-              isolated_structure: structure1.extendedData.solv_energy,
-              gain_at_complexation: -6.3,
-              p_value: structure1.pvalue,
+            structure_2: {
+              symmetry_operation: structure2.symmetry_operation,
+              symmetry_id: structure2.symmetry_id,
+              atoms: {
+                interface: structure2.int_natoms,
+                surface: structure2.extendedData.n_surface_atoms,
+                total: structure2.extendedData.total_atoms,
+              },
+              residues: {
+                interface: structure2.int_nres,
+                surface: structure2.extendedData.n_surface_residues,
+                total: structure2.extendedData.total_residues,
+              },
+              solvent_accessible_area: {
+                interface: structure2.int_area,
+                total: structure2.extendedData.asa,
+              },
+              solvation_energy: {
+                isolated_structure: structure2.extendedData.solv_energy,
+                gain_at_complexation: -4.5,
+                p_value: structure2.pvalue,
+              },
             },
           },
-          structure_2: {
-            symmetry_operation: structure2.symmetry_operation,
-            symmetry_id: structure2.symmetry_id,
-            atoms: {
-              interface: structure2.int_natoms,
-              surface: structure2.extendedData.n_surface_atoms,
-              total: structure2.extendedData.total_atoms,
-            },
-            residues: {
-              interface: structure2.int_nres,
-              surface: structure2.extendedData.n_surface_residues,
-              total: structure2.extendedData.total_residues,
-            },
-            solvent_accessible_area: {
-              interface: structure2.int_area,
-              total: structure2.extendedData.asa,
-            },
-            solvation_energy: {
-              isolated_structure: structure2.extendedData.solv_energy,
-              gain_at_complexation: -4.5,
-              p_value: structure2.pvalue,
-            },
+          interfacing_residues: {
+            structure_1: structure1Data,
+            structure_2: structure2Data,
+          },
+          interfacing_bonds: {
+            hydrogen_bonds: interfaceResult.interface.h_bonds.bonds || [],
+            salt_bridges: interfaceResult.interface.salt_bridges.bonds || [],
+            disulphide_bonds: interfaceResult.interface.ss_bonds.bonds || [],
+            covalent_links: interfaceResult.interface.cov_bonds.bonds || [],
           },
         };
 
-        console.log('Interface JSON to download:', interfaceJSON);
+        // console.log('Interface JSON to download:', interfaceJSON);
 
-        // if (interfaceResult) {
-        //   this.pisaUtilService.downloadJSON(interfaceResult, `interface-${this.interface()?.interface_id}`);
-        // }
+        if (interfaceResult) {
+          this.pisaUtilService.downloadJSON(interfaceJSON, `interface-${this.interface()?.interface_id}`);
+        }
       });
   }
 }
