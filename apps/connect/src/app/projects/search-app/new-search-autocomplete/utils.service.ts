e47e764623112aedadd4d9d8a4d123f8a821fed0
lawal <lawal@ebi.ac.uk>
2026-03-06 09:30:13 +0000

    Connect App::Search value update method fixed

--- apps/connect/src/app/projects/search-app/new-search-autocomplete/utils.service.ts
+++ apps/connect/src/app/projects/search-app/new-search-autocomplete/utils.service.ts
@@ -6,19 +6,16 @@ import { Injectable, signal } from '@angular/core';
 })
 export class UtilsService {
   public escapeValue(value: string): string {
-    // If the field value has a space, colon, quotation mark or forward slash
-    // in it, wrap it in quotes, unless it is a range query or it is already
-    // wrapped in quotes.
     if (window.location.href.indexOf('text:') < 0) {
-      if (value.match(/[ :\/"]/) && !value.match(/[\[\{]\S+ TO \S+[\]\}]/) && !value.match(/^["\(].*["\)]$/)) {
+      if (value.match(/[:\/"]/) && !value.match(/[\[\{]\S+ TO \S+[\]\}]/) && !value.match(/^["\(].*["\)]$/)) {
         return '"' + value.replace(/\\/g, '\\\\').replace(/"/g, '\\"') + '"';
       }
     } else {
-      // else if it is a text search, don't put quotes around the search term when there is a space
       if (value.match(/[:\/"]/) && !value.match(/[\[\{]\S+ TO \S+[\]\}]/) && !value.match(/^["\(].*["\)]$/)) {
         return '"' + value.replace(/\\/g, '\\\\').replace(/"/g, '\\"') + '"';
       }
     }
+
     return value;
   }
 
