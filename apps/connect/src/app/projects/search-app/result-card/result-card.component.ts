e8013b66ff3b5c9fd87b8facad60a1e82d57be21
lawal <lawal@ebi.ac.uk>
2026-03-11 11:14:09 +0000

    Connect App::Updates

--- apps/connect/src/app/projects/search-app/result-card/result-card.component.ts
+++ apps/connect/src/app/projects/search-app/result-card/result-card.component.ts
@@ -161,7 +161,7 @@ export class ResultCardComponent implements OnInit, OnChanges, OnDestroy {
     const resultData = changes['resultData']?.currentValue;
 
     //format organism Scientific Name
-    this.orgSciName = this.getOrganismScientificName(resultData.doclist.docs[0].entry_organism_scientific_name);
+    this.orgSciName = this.getOrganismScientificName(resultData?.doclist.docs[0].entry_organism_scientific_name);
   }
 
   getOrcidClaimMessageDetails() {
