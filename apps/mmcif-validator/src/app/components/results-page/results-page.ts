aa6a110a35983c43aeda3186c2cdc8f2071ffeb5
lawal <lawal@ebi.ac.uk>
2026-03-30 16:03:38 +0100

    Working in progress

--- apps/mmcif-validator/src/app/components/results-page/results-page.ts
+++ apps/mmcif-validator/src/app/components/results-page/results-page.ts
@@ -1,9 +1,15 @@
+/* eslint-disable no-useless-escape */
 import { CommonModule } from '@angular/common';
 import { Component, computed, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { MatTabChangeEvent, MatTabGroup } from '@angular/material/tabs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MaterialModule, ScrollPositionService } from '@pdbc/core';
-import { ValidationErrorItem, ValidationResult } from '../../models';
+import { MissingItem, ValidationErrorItem, ValidationResult } from '../../models';
+
+type Result = {
+  category: string;
+  items: string[];
+};
 
 @Component({
   selector: 'app-results',
@@ -29,6 +35,7 @@ export class ResultsPageComponent implements OnInit {
   public errors: ValidationErrorItem[] = [];
   public warnings: ValidationErrorItem[] = [];
   public filteredLists: ValidationErrorItem[] = [];
+  public missingCategories: Result[] = [];
 
   @ViewChild('tabs') tabGroup!: MatTabGroup;
 
@@ -52,6 +59,10 @@ export class ResultsPageComponent implements OnInit {
     this.errors = this.result?.errors?.filter((error) => error.severity === 'error') || [];
     this.warnings = this.result?.errors?.filter((error) => error.severity === 'warning') || [];
     this.filteredLists = this.result?.errors || [];
+    this.missingCategories = this.groupMissingData(
+      this.result?.metadata_completeness?.missing_categories || [],
+      this.result?.metadata_completeness?.missing_items || []
+    );
   }
 
   ngOnInit(): void {
@@ -89,5 +100,34 @@ export class ResultsPageComponent implements OnInit {
   }
   public openCompletenessTab(): void {
     this.selectedTab.set(1);
+  }
+
+  public onOpenAccordionPanel(panelName: string): void {
+    console.log(`Accordion panel opened: ${panelName}`);
+  }
+
+  private groupMissingData(missing_categories: string[], missing_items: MissingItem[]): Result[] {
+    // Normalize categories (remove brackets like "[entity_src_group]")
+    const normalizedCategories = missing_categories.map((cat) => cat.replace(/[\[\]]/g, ''));
+
+    // Initialize map
+    const categoryMap: Record<string, string[]> = {};
+
+    normalizedCategories.forEach((cat) => {
+      categoryMap[cat] = [];
+    });
+
+    // Populate map
+    missing_items.forEach(({ category, item }) => {
+      if (categoryMap[category]) {
+        categoryMap[category].push(item);
+      }
+    });
+
+    // Convert to desired array format
+    return Object.entries(categoryMap).map(([category, items]) => ({
+      category,
+      items,
+    }));
   }
 }
