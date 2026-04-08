ee87a59d27b1625791f28640a3c36388dfd28fd9
lawal <lawal@ebi.ac.uk>
2026-04-08 13:32:13 +0100

    updating!!!

--- apps/kb-corporate-page/src/app/app.config.ts
+++ apps/kb-corporate-page/src/app/app.config.ts
@@ -3,12 +3,10 @@ import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/ro
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-    { provide: APP_BASE_HREF, useValue: '/pdbe/pdbe-kb/' },
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
