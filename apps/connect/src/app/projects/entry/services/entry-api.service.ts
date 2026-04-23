742136c214e4eb79f506adfdc5a8d9405f541cbc
Adam Midlik <midlik@ebi.ac.uk>
2026-04-23 15:43:48 +0100

    Remove outdated workarounds for fetching PISA API

--- apps/connect/src/app/projects/entry/services/entry-api.service.ts
+++ apps/connect/src/app/projects/entry/services/entry-api.service.ts
@@ -325,11 +325,9 @@ export class EntryApiService {
   }
 
   public getPisaAssembly(entryId: string, assemblyId: string): Observable<PisaAssembly> {
-    // return this.http.get<Record<string, PisaAssembly>>(`${this.BASE_API_V2}pisa/assembly/${entryId}/${assemblyId}`).pipe(
-    return this.http.get<Record<string, PisaAssembly>>(`https://www.ebi.ac.uk/pdbe/api/pisa/assembly/${entryId}/${assemblyId}`).pipe(
+    return this.http.get<Record<string, PisaAssembly>>(`${this.BASE_API_V2}pisa/assembly/${entryId}/${assemblyId}`).pipe(
       map((data) => data[entryId]),
       catchError((error) => {
-        return of({ empty: true } as unknown as PisaAssembly); // temporary fix because PISA API throws CORS errors, TODO: @adam revert
         if (error?.status === 404) {
           return of({ empty: true } as unknown as PisaAssembly);
         }
