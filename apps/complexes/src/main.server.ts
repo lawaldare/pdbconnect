c8ab95c5f774972a9923aca5a473bfcd39c9ffa4
lawal <lawal@ebi.ac.uk>
2026-05-22 18:32:56 +0100

    updates...

--- apps/complexes/src/main.server.ts
+++ apps/complexes/src/main.server.ts
@@ -3,8 +3,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 
-// (global as any).XMLHttpRequest = require('xhr2');
-
 const bootstrap = () => bootstrapApplication(App, config);
 
 export default bootstrap;
