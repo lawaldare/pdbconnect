cb727084ac7b752b28b8558d5969d2b8d6aa9b6f
lawal <lawal@ebi.ac.uk>
2026-05-24 11:50:11 +0100

    updatez

--- apps/complexes/src/app/app.config.server.ts
+++ apps/complexes/src/app/app.config.server.ts
@@ -5,7 +5,14 @@ import { serverRoutes } from './app.routes.server';
 import { APP_BASE_HREF } from '@angular/common';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(withRoutes(serverRoutes)), { provide: APP_BASE_HREF, useValue: '/pdbe/pdbe-kb/complexes/' }],
+  providers: [
+    provideServerRendering(withRoutes(serverRoutes)),
+    {
+      provide: APP_BASE_HREF,
+      // Read the environment variable directly at runtime. Fallback to local root if not set.
+      useValue: process.env['APP_BASE_HREF'] || '/',
+    },
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
