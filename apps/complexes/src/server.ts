c9451b41972a2f1c5ddbd4eba77682966fefe49b
lawal <lawal@ebi.ac.uk>
2026-05-27 06:06:29 +0100

    map exact angular proxy validation environment schema

--- apps/complexes/src/server.ts
+++ apps/complexes/src/server.ts
@@ -17,7 +17,9 @@ const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
+const angularApp = new AngularNodeAppEngine({
+  trustProxyHeaders: true,
+});
 
 /**
  * Serve static files from /browser
