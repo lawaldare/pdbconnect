72c4da07cae3b68135774ebdac6639e657bc9d46
lawal <lawal@ebi.ac.uk>
2026-06-04 05:11:28 +0100

    😩

--- apps/ligands/src/server.ts
+++ apps/ligands/src/server.ts
@@ -105,12 +105,12 @@ app.get('*', (req, res, next) => {
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-if (isMainModule(import.meta.url)) {
-  const port = Number(process.env['PORT']) || 4000;
-  app.listen(port, '0.0.0.0', () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
+// if (isMainModule(import.meta.url)) {
+const port = Number(process.env['PORT']) || 4000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Node Express server listening on http://localhost:${port}`);
+});
+// }
 
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
