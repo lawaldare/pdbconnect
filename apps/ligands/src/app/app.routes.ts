c2a94c1e89c8f6152b7a7715aa206faa12225511
lawal <lawal@ebi.ac.uk>
2026-06-04 05:23:38 +0100

    ok

--- apps/ligands/src/app/app.routes.ts
+++ apps/ligands/src/app/app.routes.ts
@@ -2,6 +2,17 @@ import { Route } from '@angular/router';
 import { ErrorPageComponent } from './components/error-page/error-page.component';
 
 export const appRoutes: Route[] = [
+  // 🧠 Absolute path mapping (Fixes the current compiler mismatch)
+  {
+    path: 'pdbe-srv/pdbechem/chemicalCompound/show/:ligandId',
+    loadComponent: () => import('./components/pages/main/main.component').then((m) => m.LigandsMainPageComponent),
+    title: 'PDBe-KB Ligand Pages (PDBeChem)',
+  },
+  {
+    path: 'pdbe-srv/pdbechem/chemicalCompound/error',
+    component: ErrorPageComponent,
+    title: 'Error Page',
+  },
   {
     path: 'show/:ligandId',
     loadComponent: () => import('./components/pages/main/main.component').then((m) => m.LigandsMainPageComponent),
