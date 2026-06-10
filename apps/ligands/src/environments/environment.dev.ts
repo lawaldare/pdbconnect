d63ea7051f4e3a86eca4afda0e73ea0c9d070707
lawal <lawal@ebi.ac.uk>
2026-06-10 12:20:23 +0100

    updated the environment url

--- apps/ligands/src/environments/environment.dev.ts
+++ apps/ligands/src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   debug: true,
-  baseUrl: 'http://pdbe-aggregated-api-nginx',
+  baseUrl: 'http://pdbe-aggregated-api-nginx/',
   googleAnalyticsTag: 'G-V1BCXVELRY',
   clarityProjectIdForLigandPages: 'tx7303fj1j',
   epSurveyWebhookUrl1: 'https://script.google.com/macros/s/AKfycbyFx_TZ3EbARnbEj9sc0fLQ-cfRBu2h-MLZVuocKQK6-QDuP4xrvHPhBLmxtx8MHRkj/exec',
